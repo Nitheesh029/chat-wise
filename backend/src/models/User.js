033b67e910619e79ae53bbb45ec5dbbe973a9d50
Nitheesh029 <qxnitheesh@gmail.com>
2026-01-04 21:07:20 +0530

    feat: add User model with schema definition for name, email, and password

---
+++ backend/src/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/.+@.+\..+/, "Please fill a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
